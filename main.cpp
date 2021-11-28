933aa82ffbf6ebf5d828c2f6ad5753e034f79d81
leomet07 <leomet07@gmail.com>
2021-11-28 09:13:11 -0500

    Initial

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int getInt(){
+    int i;
+    cin >> i;
+    return i;
+}
+
+long long getLongLong(){
+    long long l;
+    cin >> l;
+    return l;
+}
+
+string getString(){
+    string s;
+    cin >> s;
+    return s;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
+    cout << "Comp Template" << endl;    
+    return 0;
+
+}
