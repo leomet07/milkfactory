2abdd6c95515ab103fd821c88d5a0367be86a51a
leomet07 <leomet07@gmail.com>
2021-11-28 17:50:03 -0500

    clean code

--- main.cpp
+++ main.cpp
@@ -3,44 +3,18 @@
 using namespace std;
 
 void dfs(set<int> &visited, map<int, vector<int>> graph, int node){
-    // Do the DFS
-    // cout << "Inner DFS" << endl;
     if (visited.find(node) == visited.end()){ // if doesnt exist
         visited.insert(node);
-        // cout << "Dfs is on node: " << node <<endl;
         if (graph.count(node) > 0){
             vector<int> neighbors = graph.at(node);
-            // cout << "Found neighbors" << endl;
             for (int i = 0; i < neighbors.size(); i++){
-                int neighbor = neighbors[i];
-                // cout << "Neighbor: " << neighbor << endl;
-                
-                dfs(visited, graph, neighbor);
-                // cout << "dfs neighbor call finished??? " << endl;
-                
+                int neighbor = neighbors[i];                
+                dfs(visited, graph, neighbor);                
             }
         }
-        
     }
 }
-void printMap(map<int, vector<int>> &mp){ // & so passes by reference
-    cout << "Print map" << endl;
-    map<int, vector<int>>::iterator itr;
-    
-    for (auto itr = mp.begin();
-         itr != mp.end(); ++itr) {
-        // cout << itr->first << ": " << itr->second << endl;
-        cout << itr->first << ": ";
-        vector<int> v = itr->second;
-        for (int i = 0; i < v.size();i++){
-            int c = v[i];
-            cout<< c <<" ";
-        }
-        cout << endl;
-    }
-    cout << "End print map" << endl;
 
-}
 int main() {
     ifstream cin("factory.in");
     ofstream cout("factory.out");
@@ -52,67 +26,28 @@ int main() {
     cin >> n;
     set<int> stations;
     map<int, vector<int>> belts;
-    // bool isA = true;
-    // int a;
-    // while (stations.size() < n ){ // Recieve station data
-    //     int s;
-    //     cin >> s;
-    //     if (isA){
-    //         a = s;
-    //     }else{
-            
-    //         // cout << "S: " << s << " A: "<< a << endl;
-    //         // check if connection exists
-    //         if (belts.count(s) == 0){
-    //             belts.insert(pair<int, vector<int>>(s , {a}));
-    //             // cout << "S: " << s << " A: "<< a << endl;
-    //             // printMap(belts);
-                
-    //         } else{
-    //             vector<int> h = belts.at(s);
-    //             h.push_back(a);
-    //             // cout << "New vector len: " << h.size() << endl; 
-    //             // belts.insert(pair<int, vector<int>>(s , h));
-    //             belts[s] = h;
-    //         }
 
-    //         stations.insert(a);
-    //         stations.insert(s);
-    //     }
-    //     isA = !isA;
-    // }
-
-    for (int i = 0; i < n -1; i++){
+    for (int i = 0; i < n -1; i++){ // Get input
         int a, b;
         cin >> a;
         cin >> b;
-
         if (belts.count(b) == 0){
-                belts.insert(pair<int, vector<int>>(b , {a}));
-                // cout << "S: " << s << " A: "<< a << endl;
-                // printMap(belts);
-                
+            belts.insert(pair<int, vector<int>>(b , {a}));   
         } else{
-                vector<int> h = belts.at(b);
-                h.push_back(a);
-                // cout << "New vector len: " << h.size() << endl; 
-                // belts.insert(pair<int, vector<int>>(s , h));
-                belts[b] = h;
+            vector<int> h = belts.at(b);
+            h.push_back(a);
+            belts[b] = h;
         }
 
         stations.insert(a);
         stations.insert(b);
-
     }
 
-    // printMap(belts);
-    // cout << "------" << endl;
     // iterate through every station
     set<int>::iterator sitr;
     int min_all = -1;
     for(sitr = stations.begin(); sitr != stations.end(); sitr++){
         int b = *sitr;
-        // cout << "Current: " << b << endl;
         set<int> check = stations;
         check.erase(*sitr);
 
@@ -124,43 +59,21 @@ int main() {
         if (belts.count(b) > 0){
             set<int> visited;
             dfs(visited, belts, b );
-            // cout << "Full dfs is finished" << endl;
-            // cout << "Visited size: " << visited.size() << endl;
-            // print the set
-            set<int>::iterator vitr;
-            for (vitr= visited.begin(); vitr!= visited.end(); vitr++){
-                // cout << "Element in visited: " << *vitr << endl;
-            }
 
             for(citr = check.begin(); citr != check.end(); citr++){
                 int a = *citr;
-                // Check
-                // cout << "Citr: " << a << endl;
-                
                 if (visited.find(a) != visited.end()){
-                    // hit all
-                    // cout <<"Increment r" << endl;
                     reached++;
                 }
             }
-        } else{
-            // cout << "Does not exist so will not bother" << endl;
-        }
-
-        
-        // cout << "Reached: " << reached <<endl;
-        // cout << "---------------" << endl;
+        } 
 
         if (reached == n - 1){
-            // cout << "Inside final if" << endl;
-            // new record
             if (min_all == -1 || b < min_all){
                 min_all = b;
             }
-
         }
     }
-    // cout << "Min all: " << min_all << endl;
     cout << min_all << endl;
     return 0;
 
